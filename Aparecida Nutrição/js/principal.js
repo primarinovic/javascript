012bc370bd801cca5dadd9db371a1a5289fd60e8
primarinovic <marinovic.priscila@gmail.com>
2020-09-28 16:37:21 -0300

    calculo imc
    Pegando as informações da tabela de pacientes e calculando o IMC

--- Aparecida Nutrição/js/principal.js
+++ Aparecida Nutrição/js/principal.js
@@ -1,2 +1,67 @@
 var titulo = document.querySelector('.titulo');
-		titulo.textContent = "Aparecida Nutricionista"
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente"); //acessando as informações do paciente
+
+var tdPeso = paciente.querySelector(".info-peso"); //puxando a informação de peso
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura"); //puxando a informação da altura
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc"); //puxando a informação do imc
+tdImc.textContent = imc; //alterando o valor do imc na tabela
+
+var paciente = document.querySelector("#segundo-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#terceiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#quarto-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#quinto-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
