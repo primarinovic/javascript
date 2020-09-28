34f8a86af22dbb528c8208513ce43f94dcbe81e0
primarinovic <marinovic.priscila@gmail.com>
2020-09-28 17:04:11 -0300

    Verificando altura e peso
    Verificação se peso e altura são valores válidos

--- Aparecida Nutrição/js/principal.js
+++ Aparecida Nutrição/js/principal.js
@@ -9,10 +9,32 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura"); //puxando a informação da altura
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
-
 var tdImc = paciente.querySelector(".info-imc"); //puxando a informação do imc
-tdImc.textContent = imc; //alterando o valor do imc na tabela
+
+var pesoValido = true;
+var alturaValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+
+	console.log("Peso inválido");
+	pesoValido = false;
+	tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+
+	console.log("Altura inválida");
+	alturaValida = false;
+	tdImc.textContent = "Altura inválida";
+}
+
+if (pesoValido && alturaValida) {
+
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc; //alterando o valor do imc na tabela
+}
+
+//Segundo paciente
 
 var paciente = document.querySelector("#segundo-paciente");
 
@@ -27,6 +49,8 @@ var imc = peso / (altura * altura);
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
 
+//Terceiro paciente
+
 var paciente = document.querySelector("#terceiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
@@ -40,6 +64,8 @@ var imc = peso / (altura * altura);
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
 
+//Quarto paciente
+
 var paciente = document.querySelector("#quarto-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
@@ -52,6 +78,8 @@ var imc = peso / (altura * altura);
 
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
+
+//Quinto paciente
 
 var paciente = document.querySelector("#quinto-paciente");
 
